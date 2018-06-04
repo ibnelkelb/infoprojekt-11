a10b282cf7f3f47b733f123bda68730098ae3ab7
n.abderrazzag <n.abderrazzag@IR1-PC-05.musterschule.schule.paedml>
2018-06-04 09:08:54 +0200

    jeder spieltag 1 mannschaft doppelt im 1. und 3. spiel

--- src/infoprojekt11/Infoprojekt11.java
+++ src/infoprojekt11/Infoprojekt11.java
@@ -39,29 +39,29 @@ public class Infoprojekt11 {
         k[6] = 7;
         k[7] = (n/2)-1;
         
-        for (int i=1; i<=18; i++){
+        for (int i=1; i<=17; i++){
         Spieltag s_i = new Spieltag();
         
         s_i.mannschaft18 = mannschaft[n-1];
         s_i.mannschaft1  = mannschaft[i-1];
-        s_i.mannschaft2  = mannschaft[(i+k[0])%(n-1)];
-        s_i.mannschaft17 = mannschaft[(i-k[0])%(n-1)];
-        s_i.mannschaft3  = mannschaft[(i+k[1])%(n-1)];
-        s_i.mannschaft16 = mannschaft[(i-k[1])%(n-1)];
-        s_i.mannschaft4  = mannschaft[(i+k[2])%(n-1)];
-        s_i.mannschaft15 = mannschaft[(i-k[2])%(n-1)];
-        s_i.mannschaft5  = mannschaft[(i+k[3])%(n-1)];
-        s_i.mannschaft14 = mannschaft[(i-k[3])%(n-1)];
-        s_i.mannschaft6  = mannschaft[(i+k[4])%(n-1)];
-        s_i.mannschaft13 = mannschaft[(i-k[4])%(n-1)];
-        s_i.mannschaft7  = mannschaft[(i+k[5])%(n-1)];
-        s_i.mannschaft12 = mannschaft[(i-k[5])%(n-1)];
-        s_i.mannschaft8  = mannschaft[(i+k[6])%(n-1)];
-        s_i.mannschaft11 = mannschaft[(i-k[6])%(n-1)];
-        s_i.mannschaft9  = mannschaft[(i+k[7])%(n-1)];
-        s_i.mannschaft10 = mannschaft[(i-k[7])%(n-1)];    
+        s_i.mannschaft2  = mannschaft[(i+k[0]+n)%(n-1)];
+        s_i.mannschaft17 = mannschaft[(i-k[0]+n)%(n-1)];
+        s_i.mannschaft3  = mannschaft[(i+k[1]+n)%(n-1)];
+        s_i.mannschaft16 = mannschaft[(i-k[1]+n)%(n-1)];
+        s_i.mannschaft4  = mannschaft[(i+k[2]+n)%(n-1)];
+        s_i.mannschaft15 = mannschaft[(i-k[2]+n)%(n-1)];
+        s_i.mannschaft5  = mannschaft[(i+k[3]+n)%(n-1)];
+        s_i.mannschaft14 = mannschaft[(i-k[3]+n)%(n-1)];
+        s_i.mannschaft6  = mannschaft[(i+k[4]+n)%(n-1)];
+        s_i.mannschaft13 = mannschaft[(i-k[4]+n)%(n-1)];
+        s_i.mannschaft7  = mannschaft[(i+k[5]+n)%(n-1)];
+        s_i.mannschaft12 = mannschaft[(i-k[5]+n)%(n-1)];
+        s_i.mannschaft8  = mannschaft[(i+k[6]+n)%(n-1)];
+        s_i.mannschaft11 = mannschaft[(i-k[6]+n)%(n-1)];
+        s_i.mannschaft9  = mannschaft[(i+k[7]+n)%(n-1)];
+        s_i.mannschaft10 = mannschaft[(i-k[7]+n)%(n-1)];    
         
-        System.out.println("Spieltag"+i);
+        System.out.println("\n\nSpieltag"+i);
         
         if(i%2==0){
             System.out.println(s_i.mannschaft18+"-"+s_i.mannschaft1);
